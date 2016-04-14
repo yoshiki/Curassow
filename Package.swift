c22e8cb1ba5ab060665dc11e4bfe8159b0918bdb
Yoshiki Kurihara <clouder@gmail.com>
2016-04-14 23:39:04 +0900

    update fd source

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 3),
     .Package(url: "https://github.com/yoshiki/Inquiline.git", majorVersion: 0, minor: 3),
     .Package(url: "https://github.com/yoshiki/Commander.git", majorVersion: 0, minor: 4),
-    .Package(url: "https://github.com/kylef/fd.git", majorVersion: 0, minor: 1),
+    .Package(url: "https://github.com/yoshiki/fd.git", majorVersion: 0, minor: 1),
   ],
   testDependencies: [
     .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 6),
